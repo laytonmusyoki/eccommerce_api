fcd2b9bdd17e67d7d211aa6a37ed209975d55e6d
layton <127030731+laytonmusyoki@users.noreply.github.com>
2024-05-30 15:58:46 +0300

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -32,24 +32,24 @@ def login_user(request):
             "message": serializer.errors
         }, status=status.HTTP_400_BAD_REQUEST)
     
-    user = authenticate(username=serializer.data['username'], password=serializer.data['password'])
-    
-    if not user:
-        return Response({
-            "status": 400,
-            "message": 'Invalid credentials'
-        }, status=status.HTTP_400_BAD_REQUEST)
-    
-    refresh = RefreshToken.for_user(user)
-    access_token = str(refresh.access_token)
-    refresh_token = str(refresh)
-    
-    return Response({
-        'status': 200,
-        'access': access_token,
-        'refresh': refresh_token
-    }, status=status.HTTP_200_OK)
-
+    else:
+        user = authenticate(username=serializer.data['username'], password=serializer.data['password'])
+        if not user:
+            return Response({
+                "status": 400,
+                "message": 'Invalid credentials'
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        else:
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+            refresh_token = str(refresh)
+            
+            return Response({
+                'status': 200,
+                'access': access_token,
+                'refresh': refresh_token
+            }, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 def add_product(request):
