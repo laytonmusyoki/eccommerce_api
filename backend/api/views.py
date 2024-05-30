cc72d7f0966fd533afd742ccfc8d8afb3d942d29
layton <laytonmatheka@gmail.com>
2024-05-30 16:05:33 +0300

    login error

--- backend/api/views.py
+++ backend/api/views.py
@@ -32,24 +32,23 @@ def login_user(request):
             "message": serializer.errors
         }, status=status.HTTP_400_BAD_REQUEST)
     
-    else:
-        user = authenticate(username=serializer.data['username'], password=serializer.data['password'])
-        if not user:
-            return Response({
-                "status": 400,
-                "message": 'Invalid credentials'
-            }, status=status.HTTP_400_BAD_REQUEST)
-        
-        else:
-            refresh = RefreshToken.for_user(user)
-            access_token = str(refresh.access_token)
-            refresh_token = str(refresh)
-            
-            return Response({
-                'status': 200,
-                'access': access_token,
-                'refresh': refresh_token
-            }, status=status.HTTP_200_OK)
+    user = authenticate(username=serializer.data['username'], password=serializer.data['password'])
+    
+    if not user:
+        return Response({
+            "status": 400,
+            "message": 'Invalid credentials'
+        }, status=status.HTTP_400_BAD_REQUEST)
+    
+    refresh = RefreshToken.for_user(user)
+    access_token = str(refresh.access_token)
+    refresh_token = str(refresh)
+    
+    return Response({
+        'status': 200,
+        'access': access_token,
+        'refresh': refresh_token
+    }, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 def add_product(request):
